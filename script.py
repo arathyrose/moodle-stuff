f999289054808ed9a66d23b2ee57aa63a277cd8f
arathyrose <arathyrose2000@gmail.com>
2020-01-07 13:58:39 +0530

    Get attendacne of each course

--- script.py
+++ script.py
@@ -29,7 +29,6 @@ driver.find_element(By.ID, "password").send_keys(Pass)
 driver.find_element(By.NAME, "submit").click()
 
 # get the list of all courses
-
 i=1
 list_courses= []
 print("\r",end="")
@@ -40,5 +39,30 @@ while True:
         i+=1
     except:
         break
+print("All courses taken this semester are: ")
+print()
+for course in list_courses:
+    print(course)
+print()
 
-print(list_courses)
+# get attendance of each course
+print('Attendance per course is:')
+driver.find_element(By.XPATH, "/html/body/div[4]/div/div/div/section/div/aside/div/div[2]/div/div[6]/div[1]/h2/a").click() # select a random course
+driver.find_element(By.XPATH, "//span[contains(.,'Attendance')]").click() # click on its attendance
+driver.find_element(By.LINK_TEXT, "All courses").click() # click on every other course
+i = 1
+while True:
+    try:
+        cname = driver.find_element(
+            By.XPATH, '/html/body/div[4]/div/div/div/section/div/table/tbody/tr/td[2]/h3['+str(i)+']').text
+        if cname in list_courses:
+            tot = driver.find_element(
+                By.XPATH, '/html/body/div[4]/div/div/div/section/div/table/tbody/tr/td[2]/table['+str(i)+']/tbody/tr[1]/td[2]').text
+            att = driver.find_element(
+                By.XPATH, '/html/body/div[4]/div/div/div/section/div/table/tbody/tr/td[2]/table['+str(i)+']/tbody/tr[2]/td[2]').text
+            print(cname+":" + att + "/"+tot)
+        i += 1
+    except:
+        break
+
+driver.quit()
